2f72fc19ba81347fcf6b128546e140aa9db30a0d
Devo Astroman <devo.astroman@gmail.com>
2025-10-30 11:17:25 +0100

    add hero horizontal movement

--- Assets/Scripts/Hero/HeroHorizontalMovement.cs
+++ Assets/Scripts/Hero/HeroHorizontalMovement.cs
@@ -2,8 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class HeroHorizontalMovement : MonoBehaviour
 {
+
+    [Header("Horizontal Movement")]
+    [SerializeField] private float horizontalSpeed = 5f;        // Optional left/right move (A/D or arrows)
+
+    private Rigidbody2D rb;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();       
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,11 @@ public class HeroHorizontalMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Horizontal input (optional)
+        float x = Input.GetAxisRaw("Horizontal"); // A/D or arrows
+        Vector2 v = rb.velocity;
+        v.x = x * horizontalSpeed;
+        rb.velocity = new Vector2(v.x, rb.velocity.y);
         
     }
 }
